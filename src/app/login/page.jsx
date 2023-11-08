4f8c377e8de0c347274d2366ba8bf8745abe8d74
guinaoki <rm551456@fiap.com.br>
2023-11-08 11:28:49 -0300

    Rodape e Form de Login estilizado

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,26 +1,84 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
 
 export default function Login() {
+    
+    const [usuario, setUsuario] = useState({
+        "email" : "",
+        "senha" : ""
+    })
+
+    const [msgStatus, setMsgStatus] = useState("");
+    const [classeMsg, setClasseMsg] = useState("");
+
+    useEffect(() => {
+      //Só será executado, caso o estado do msgStatus se alterar.
+        if(msgStatus == "Login realizado com SUCESSO!"){
+            setClasseMsg("login-sucesso");
+        }else if(msgStatus == "Nome de usuário ou senha inválidos!"){
+            setClasseMsg("login-erro");
+        }else{
+            setClasseMsg("login-none");
+        }
+
+    }, [msgStatus])
+          
+    const handleChange = async (e)=>{
+        const {name, value} = e.target;
+    setUsuario({...usuario,[name]:value});
+    };
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:3000/api/base/base-users",{
+                method: "POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:  JSON.stringify(usuario)
+            });
+
+            if(response.ok){
+                const status = await response.json();
+                if(status.status){
+                   setMsgStatus("Login realizado com SUCESSO!");
+                }else{
+                    setMsgStatus("Nome de usuário ou senha inválidos!");
+                    setUsuario({
+                        "email":"",
+                        "senha":""
+                    });
+                }
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+        
+    };
+    
+
   return (
     <div>
-        <h1>Identifucação de Usuários</h1>
+        <h1 className="text-4xl text-center p-2 font-extrabold">Identificação de Usuários</h1>
         <h2>-</h2>
-        <div>
-            <form className='form-login'>
+        <div className="form-login">
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>LOGIN</legend>
                     <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name='email' id='idEmail'
-                        placeholder='Digite seu Email.' value=""onChange=""/>
+                        <label htmlFor="idEmail">Email</label>
+                        <input type="email" name="email" id="idEmail" placeholder="Digite seu Email." value={usuario.email} onChange={handleChange}/>
                     </div>
                     <div>
-                        <label htmlFor="senha">Senha</label>
-                        <input type="senha" name='senha' id='idSenha'/>
-                        <input type="senha" name='senha' id='idSenha'
-                        placeholder='Digite sua Senha.' value=""onChange=""/>
+                        <label htmlFor="idSenha">Senha</label>
+                        <input type="password" name="senha" id="idSenha" placeholder="Digite sua Senha." value={usuario.senha} onChange={handleChange}/>
                     </div>
-                    <div>LOGIN</div>
+                    <div>
+                        <button>LOGIN</button> 
+                    </div>
                 </fieldset>
             </form>
         </div>
